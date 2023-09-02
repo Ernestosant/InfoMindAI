d5e4298403d86085c69338f7c92153318f324bbe
Ernesto Santiesteban <ernestosantiesteban5@gmail.com>
2023-09-02 16:51:45 -0700

    revert to first stable version

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ from langchain.agents import create_csv_agent
 from langchain.agents.agent_types import AgentType
 import shutil
 
-
-
+# Limpiando archivos temporales
+shutil.rmtree('pdfs')
+shutil.rmtree('data_files')
 
 
 #***********************Functions***************************************************************************************
@@ -126,20 +127,6 @@ with st.sidebar:
     except:
         st.write('There is not a data file')
 
-
-# Cleaning temp directories
-if not(apikey):
-    try:
-        if os.path.exists('pdfs') and os.path.isdir('pdfs'):
-            shutil.rmtree('pdfs')
-    except Exception as e:
-        print(f"An error occurred while deleting 'pdfs': {e}")
-
-    try:
-        if os.path.exists('data_files') and os.path.isdir('data_files'):
-            shutil.rmtree('data_files')
-    except Exception as e:
-        print(f"An error occurred while deleting 'data_files': {e}")
 
 # sesions states trackers
 
